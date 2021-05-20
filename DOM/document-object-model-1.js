584f5aea1f06967a426ab5413ce1b7e88473e24e
justcodingman <joginder9868@outlook.com>
2021-05-20 16:16:32 +0530

    Initial commit

---
+++ DOM/document-object-model-1.js
@@ -0,0 +1,14 @@
+// change all the HTML element in the page
+// change all the HTML attributes in the page
+//change all the CSS styles in the page
+//remove existing HTML elements and attributes
+//Add new HTML elements and attributes
+//javascript can react to all existing HTML events in the page
+//javascript can create new HTML events in the page
+
+//document.write("hello");
+
+//write in console.log
+
+//window
+//window.alert("warning");

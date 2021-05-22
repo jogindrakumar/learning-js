f3cfd2a3ef31402098443c93e634cfa09975af66
justcodingman <joginder9868@outlook.com>
2021-05-22 18:18:15 +0530

    new file:   DOM/dom-selectors.html 	new file:   DOM/dom-selectors.js

---
+++ DOM/dom-selectors.js
@@ -0,0 +1,32 @@
+//DOM Selectors
+// getElementByTagName
+//run code in console for pratice and target element
+console.log(document.getElementsByTagName("h1")[0]);
+
+//getElementByClassName
+console.log(document.getElementsByClassName("second")[0]); // access the value inside of it use index[0]
+
+//getElementById
+console.log(document.getElementById("first")); // already gave us value no need to use index for this one because id is unique and doesn't have index number whereas class and tag are more in HTML so we used index[] to access their value
+
+//querySelector
+//querySelectorAll
+
+//getAttribute
+//setAttribute
+
+// ## Changing Style(css)
+//Style.{property} ok
+
+//className  --best
+//classList  --best
+
+//classList.add
+//classList.remove
+//classList.toggle
+
+//## Bonus
+// innerHTML  ---- Dangerous
+
+//parentElement
+//children
